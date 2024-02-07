f242f2c3525efc56a7fe8286a23a636881f2e207
Maxime KUIL <maximekuil@gmail.com>
2024-02-07 16:32:22 +0100

    perform

--- utils.py
+++ utils.py
@@ -10,6 +10,19 @@ def feature_importance(X, y, model, pipe):
     feature_importances = np.round((model.feature_importances_ / sum(model.feature_importances_)) * 100, 2)
 
     f = pd.DataFrame({'features': np.array(list(feat_labels)), 'score %': feature_importances})
-    f.sort_values(by=['score %'], ascending=False, inplace=True)
+    f.sort_values(by=['score %'], ignore_index=True, ascending=False, inplace=True)
+    f = f.reset_index(drop=True)
     
     return f, X_train_transf
+
+def transform_curracy(df, columns):
+    for col in columns:
+        df[col] = df[col].replace(r'[\$ ,]', '', regex=True).astype(float)
+
+def transform_date(df, columns, format='%d-%b-%y'):
+    for col in columns:
+        try:
+            df[col] = pd.to_datetime(df[col], format=format, errors='raise')
+        except ValueError:
+            print('Erreur sur la date col:', col)
+            pass
